6ffdf50877c21705086a24e1b1b3d3a0daa1da07
Ashley Bloodgood <114004494+abloodgood@users.noreply.github.com>
2022-12-10 10:31:41 -0500

    FORMATTING

--- validate.js
+++ validate.js
@@ -7,24 +7,23 @@ function checkvalue(val)
 }
 
 function validateForm() 
-{
-    	
-		var name = document.getElementById("name");
-		/*Check to make sure first and last name are both over 2 characters*/
-		if (name.value.length <= 2) {
-					alert("Name must contain two or more characters!");
-					name.style.borderColor = "red";
-					return false;
-		}
+{	
+	var name = document.getElementById("name");
+	/*Check to make sure first and last name are both over 2 characters*/
+	if (name.value.length <= 2) {
+		Alert("Name must contain two or more characters!");
+		name.style.borderColor = "red";
+		return false;
+	}
 		
-		/*use reg ex to test if first or last name have anything other than letters*/
-		var letters = /^[a-zA-Z]+$/;
-		if (!letters.test(name.value)) {
-					alert("Names can only contain letters!");
-					firstName.style.borderColor = "red";
-					lastName.style.borderColor = "red";
-					return false;
-		}
+	/*use reg ex to test if first or last name have anything other than letters*/
+	var letters = /^[a-zA-Z]+$/;
+	if (!letters.test(name.value)) {
+		alert("Names can only contain letters!");
+		firstName.style.borderColor = "red";
+		lastName.style.borderColor = "red";
+		return false;
+	}
     var phone = document.getElementById("phone");
     var phoneRegExp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 

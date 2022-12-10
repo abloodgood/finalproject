e6cb339aa2ab5313289041085d773de9bd41b416
Ashley Bloodgood <114004494+abloodgood@users.noreply.github.com>
2022-12-10 10:27:25 -0500

    fixing

--- validate.js
+++ validate.js
@@ -8,11 +8,29 @@ function checkvalue(val)
 
 function validateForm() 
 {
-    email = document.getElementById("email");
+    	
+		var name = document.getElementById("name");
+		/*Check to make sure first and last name are both over 2 characters*/
+		if (name.value.length <= 2) {
+					alert("Name must contain two or more characters!");
+					name.style.borderColor = "red";
+					return false;
+		}
+		
+		/*use reg ex to test if first or last name have anything other than letters*/
+		var letters = /^[a-zA-Z]+$/;
+		if (!letters.test(name.value)) {
+					alert("Names can only contain letters!");
+					firstName.style.borderColor = "red";
+					lastName.style.borderColor = "red";
+					return false;
+		}
+    var phone = document.getElementById("phone");
+    var phoneRegExp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
-    if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(email))) {
-        alert("Please enter a valid email address.")
-        email.style.borderColor = "red";
+    if (!(phoneRegExp.test(phone))) {
+        alert("Please enter a valid phone number.")
+        phone.style.borderColor = "red";
         return false;
     }
     else {
